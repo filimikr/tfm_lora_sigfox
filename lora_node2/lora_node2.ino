14074f9dab06d830ea1607dc291c7c0c57380e8f
Filippos Mikropoulos <filimikr@teiemt.gr>
2021-02-15 21:26:32 +0100

    minor fixes - tests more sensors

--- lora_node2/lora_node2.ino
+++ lora_node2/lora_node2.ino
@@ -9,17 +9,16 @@ int tx_time = 10000; //TX time periodicity
 
 //Leaf sensors Setup
 int leafSupply = 4; //digital port 2 supply
-//declare analogRead ports for each sensor
+//set analogRead ports for each sensor
 int leaf0 = 0; //First leaf will send output to port A0 and so on
-//int leaf1 = 1; //uncomment if additional sensors are added
-//int leaf2 = 2;
-//int leaf3 = 3;
+int leaf1 = 1; //uncomment if additional sensors are added
+int leaf2 = 2;
+int leaf3 = 3;
 //int leaf4 = 4;
 //int leaf5 = 5;
-const unsigned int sensors = 1; //number of leaf sensors
-int leafs[sensors] = {leaf0}; //add the analogRead ports in the list, IF MORE SENSORS ARE ADDED, add more like {leaf0, leaf1,...}
+const unsigned int sensors = 4; //number of leaf sensors
+int leafs[sensors] = {leaf0, leaf1, leaf2, leaf3}; //add the analogRead ports in the list, IF MORE SENSORS ARE ADDED, add more like {leaf0, leaf1,...}
 unsigned int leafMeasure[sensors]; //set measurements list length
-//unsigned int finalMeasureToPayload[sensors]; //initialize array to store measurements converted to percentage and 8bit
 
 uint8_t payload[sensors+1]; //set payload list length (The length will be the number of sensors +1 for the device ID)
 uint8_t dev_id = 132; //unique device ID
@@ -54,7 +53,7 @@ void loop() {
           if (buf[1] != 0) {
             //tx_time = (buf[1]*60) / 8; //updating tx time interval
             if (buf[1] == 15) { //payload
-              Serial.println("Receiveard Downlink Data!: ");
+              Serial.println("Received Downlink Data!: ");
               Serial.println(buf[0]);                                       
               Serial.println(buf[1]);
             }
@@ -73,9 +72,8 @@ void loop() {
     }
   }
 
- // Serial.println("Lets Sleep...");
-  //delay(4 * 60 * 1000); //4mins
-  delay(240000);
+  Serial.println("Lets Sleep...");
+  delay(240000); //4 mins
   /* Activate this when setting the low power idle mode
     for (tx_time; tx_time > 0; sleepCounter--){
     LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
@@ -83,6 +81,7 @@ void loop() {
 }
 
 void preparePayload() {
+  payload[0] = dev_id;
   digitalWrite(leafSupply, HIGH); // sets the digital pin 2 on
   delay(100);
   
@@ -100,16 +99,7 @@ void preparePayload() {
     Serial.print(leafMeasure[i]);
     Serial.print(" --> ");
     Serial.print(payload[i+1]);
-    //Serial.print(finalMeasureToPayload[i]);
     Serial.println("%");
-    
-    //payload[i+1] = finalMeasureToPayload[i]; // We go +1 because in payload[0] we will store the device ID
-    
  }
-  //delay(100);
-  //digitalWrite(leafSupply, LOW);  // sets the digital pin off
-  //uint8_t wetness[i+1] = wet_percent[i];
-  payload[0] = dev_id;
-  //payload[1] = wetness;
-  //payload[2] = 0;
+  digitalWrite(leafSupply, LOW);  // sets the digital pin off
 }
